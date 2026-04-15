69df06e5d6c737f8ae6d4a2002d9071dd984879d
Les134 <admin@autofest.com.au>
2026-04-15 14:29:43 +1000

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCuvxgmnpr8EVRAN9y-AJCWGVOf0xi7sxk",
+  authDomain: "autofestscoreapp.firebaseapp.com",
+  projectId: "autofestscoreapp",
+  storageBucket: "autofestscoreapp.firebasestorage.app",
+  messagingSenderId: "293382914162",
+  appId: "1:293382914162:web:1c0ab2f637988819f9c2eb"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);

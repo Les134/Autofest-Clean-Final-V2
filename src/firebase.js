0869f43ddbbb08b801ccfd66b15a67ad2ac997ba
Les134 <admin@autofest.com.au>
2026-04-16 13:55:17 +1000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,14 @@
 import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  enableIndexedDbPersistence
-} from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "YOUR_KEY",
   authDomain: "autofestscoreapp.firebaseapp.com",
   projectId: "autofestscoreapp",
   storageBucket: "autofestscoreapp.appspot.com",
-  messagingSenderId: "YOUR_ID",
-  appId: "YOUR_APP_ID"
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// ✅ SAFE (won’t crash app if it fails)
-enableIndexedDbPersistence(db).catch((err) => {
-  console.log("Persistence error:", err);
-});
-
-export { db };
+export const db = getFirestore(app);

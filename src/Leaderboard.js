aa41e4f7b1b8401bcf11fcb57249b201424c2dbc
Les134 <admin@autofest.com.au>
2026-04-29 10:10:09 +1000

    Update Leaderboard.js

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -1,17 +1,10 @@
- return (
+import React from "react";
+
+export default function Leaderboard({ eventName }) {
+  return (
     <div style={{ marginTop: 40 }}>
       <h2>Leaderboard</h2>
-
-      {Object.keys(data).map((cls) => (
-        <div key={cls}>
-          <h3>{cls}</h3>
-          {data[cls].map((car, i) => (
-            <div key={i}>
-              {i + 1}. {car.carName} - {car.total}
-            </div>
-          ))}
-        </div>
-      ))}
+      <p>Live results will appear here.</p>
     </div>
   );
 }

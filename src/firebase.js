bdfeda82290473c10f2f1909c9240bdafdfbf38b
Les134 <admin@autofest.com.au>
2026-04-30 15:11:28 +1000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,10 @@ const firebaseConfig = {
   messagingSenderId: "453347070025",
   appId: "1:453347070025:web:0567bc51df8a0b49b46f98"
 };
-
+{
+  "eventName": "Springnats",
+  "locked": false
+}
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 

2230792acf40a8dbdecf5c267c53845f3f3ca4e5
Les134 <admin@autofest.com.au>
2026-04-30 12:58:33 +1000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_REAL_KEY",
+  apiKey: "YOUR REAL KEY",
   authDomain: "autofest-burnout-judging.firebaseapp.com",
   projectId: "autofest-burnout-judging",
-  storageBucket: "autofest-burnout-judging.appspot.com",
-  messagingSenderId: "PASTE_REAL_ID",
-  appId: "PASTE_REAL_APP_ID"
+  storageBucket: "autofest-burnout-judging.firebasestorage.app",
+  messagingSenderId: "453347070025",
+  appId: "1:453347070025:web:0567bc51df8a0b49b46f98"
 };
 
 const app = initializeApp(firebaseConfig);
